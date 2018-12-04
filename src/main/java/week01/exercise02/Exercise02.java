9ef16a973e7e83c996dc7e78ddeece2da7412f12
Axel Schwarz <a.schwarz@telekom.de>
2018-12-04 12:06:31 +0100

    Zweite Advent

--- src/main/java/week01/exercise02/Exercise02.java
+++ src/main/java/week01/exercise02/Exercise02.java
@@ -1,9 +1,10 @@
 package week01.exercise02;
 
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.text.WordUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -18,7 +19,18 @@ public class Exercise02 {
     public String run(String text) {
         log.info(String.format("Run(\"%s\")", text));
 
-        return text;
+        String[] array = text.split(" ");
+        List<String> list = new ArrayList<>();
+
+        for (String word : array) {
+            char[] wordArrayCapitalize = word.toCharArray();
+            wordArrayCapitalize[0] = Character.toUpperCase(wordArrayCapitalize[0]);
+            list.add(new String(wordArrayCapitalize));
+        }
+
+        String string = String.join(" ", list);
+        return string;
     }
+
 }
 
