30382a9afcb2c108a75d097352202d3c40fc12a1
[pegeme] <[merkelp@telekom.de]>
2018-12-06 14:45:54 +0100

    Aufgabe 4 gelöst

--- src/main/java/week01/exercise04/Exercise04.java
+++ src/main/java/week01/exercise04/Exercise04.java
@@ -11,11 +11,54 @@ import java.util.logging.Logger;
  *
  */
 public class Exercise04 {
-    private static final Logger log = Logger.getLogger( Exercise04.class.getName() );
+    //private static final Logger log = Logger.getLogger( Exercise04.class.getName() );
 
-    public String run(String text) {
-        log.info(String.format("Run(\"%s\")", text));
 
-        return text;
+    public String cleanText(String parameterTextCleanText){
+
+        parameterTextCleanText = parameterTextCleanText.replaceAll("&", "");
+        parameterTextCleanText = parameterTextCleanText.replaceAll("!", "");
+
+        return parameterTextCleanText;//verbindung herstellen fuer beide Methoden
     }
+
+    public String run(String parameterTextRun) {
+        //log.info(String.format("Run(\"%s\")", text));
+        parameterTextRun = cleanText(parameterTextRun);
+        //ich rufe die Methode getWords auf und übergebe den Wert von ParameterTextRun,
+        // danach weise  ich den Rückgabewert von getWords der variabeln Liste zu.
+        String[] liste = getWords(parameterTextRun);
+        // Zaehle Buchstaben
+        int indexzaehler=0;
+        int vorherigeLaenge = 0;
+        String hilfsVariabel = "";
+        for(; indexzaehler < liste.length; indexzaehler++) {
+            //System.out.println(indexzaehler);
+            String aktuellerWert = liste[indexzaehler];
+            //System.out.println(aktuellerWert);
+            liste[indexzaehler].length();
+            //System.out.println(aktuellerWert.length());
+            if (aktuellerWert.length() > vorherigeLaenge) {
+                hilfsVariabel = aktuellerWert;
+                vorherigeLaenge = aktuellerWert.length();
+
+
+            }
+
+        }
+
+        // Vergleiche Anzahl Buchstaben
+
+        return hilfsVariabel;
+    }
+
+    public String[] getWords(String parameterTextGetWords) {
+            String[] arrayOfWords = parameterTextGetWords.split(" ");
+
+
+        return arrayOfWords;
+
+    }
+
+
 }
