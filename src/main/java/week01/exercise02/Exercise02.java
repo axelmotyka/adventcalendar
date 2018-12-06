b3c56d6cdbce8df4a529d0144031329c3a64f79a
MesutCevik <mesut.cevik@gmx.de>
2018-12-06 14:20:59 +0100

    Revised Exercise01.

--- src/main/java/week01/exercise02/Exercise02.java
+++ src/main/java/week01/exercise02/Exercise02.java
@@ -10,11 +10,22 @@ import java.util.logging.Logger;
  * Words will be separated by only one space.
  */
 public class Exercise02 {
-    private static final Logger log = Logger.getLogger( Exercise02.class.getName() );
 
     public String run(String text) {
-        log.info(String.format("Run(\"%s\")", text.toUpperCase()));
 
-        return text.toUpperCase();
+        // Wandle String aus "text" in ein Array (Liste) von Buchstaben um.
+        char[] buchstaben = text.toCharArray();
+
+        for (int i = 0; i < buchstaben.length; i++) {
+            // Leerstellen suchen.
+            char buchstabe = buchstaben[i];
+            if (buchstabe == ' ') {
+                // Erhöhe den Index i um 1, um das Zeichen nach der Leerstelle zu erhalten
+                i = i + 1;
+                // Nehme damit den Buchstaben nach der Leerstelle und wandle es in UpperCase um
+                buchstaben[i] = Character.toUpperCase(buchstaben[i]);
+            }
+        }
+        return new String(buchstaben);
     }
 }
--- src/test/java/week01/exercise01/Exercise01Test.java
+++ src/test/java/week01/exercise01/Exercise01Test.java
@@ -4,14 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import week01.exercise01.Exercise01;
-
 public class Exercise01Test {
 
     @Test
     public void runIsReturningHelloDevAcademy() {
         Exercise01 tester = new Exercise01();
 
-        assertTrue("Run method is always returning true", tester.run());
+        String expected = "HelloDevAcademy";
+        String actual = tester.run();
+
+        assertEquals(expected, actual);
     }
 }
