da8eaee015ccf7d84ffaea36125e23b937478cf2
fldtag <lutzf@telekom.de>
2018-12-06 12:05:17 +0100

    Exercise 1

--- src/main/java/week01/exercise02/Exercise02.java
+++ src/main/java/week01/exercise02/Exercise02.java
@@ -10,11 +10,20 @@ import java.util.logging.Logger;
  * Words will be separated by only one space.
  */
 public class Exercise02 {
-    private static final Logger log = Logger.getLogger( Exercise02.class.getName() );
+    private static final Logger log = Logger.getLogger(Exercise02.class.getName());
 
     public String run(String text) {
         log.info(String.format("Run(\"%s\")", text));
 
-        return text;
+        String capText = "" + Character.toUpperCase(text.charAt(0));
+        for (int i = 1; i < text.length(); i++) {
+            if (text.charAt(i-1) == (' ')) {
+                capText += Character.toUpperCase(text.charAt(i));
+            } else {
+                capText += text.charAt(i);
+            }
+        }
+        return capText;
+        }
     }
-}
+
