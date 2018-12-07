201ea74a50518a998638419e49d125d180b1f249
MesutCevik <mesut.cevik@gmx.de>
2018-12-07 09:54:39 +0100

    Info for Florian: Work in progress.

--- src/main/java/week01/exercise05/Exercise05.java
+++ src/main/java/week01/exercise05/Exercise05.java
@@ -14,7 +14,18 @@ public class Exercise05 {
 
     public String run(String text) {
         log.info(String.format("Run(\"%s\")", text));
-
+/*
+1) Slice the text being passed into characters and put them in a list variable with characters "textChar".
+2) Iterate through "textChar" and proof, if a character is from the set [a-z] or [A-Z].
+(if(Character.isLetter(textChar[i])))
+3) If yes, than replace the character through the character, which follows it in the alphabet.
+For this, determine the ASCII-number of the character and the folowing character.
+4) Iterate through the new list of characters and look for vowels (a, e, i, o, u).
+5) If the character is a vowel, then change it to upper case letter (something like:
+buchstaben[i] = Character.toUpperCase(buchstaben[i]))
+6) At least concatenate the changed characters to new string "textChanged".
+7) Return the result and print it out.
+ */
         return text;
     }
 }
