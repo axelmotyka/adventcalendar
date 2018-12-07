b2a77d1522af5bebaaf1dfb73a4c398533d09770
Axel Motyka <axel.motyka@googlemail.com>
2018-12-07 14:53:39 +0100

    push

--- src/main/java/week01/exercise10/Exercise10.java
+++ src/main/java/week01/exercise10/Exercise10.java
@@ -4,40 +4,132 @@ package week01.exercise10;
  * Closest Enemy
  * Write a small calculator application, which takes a String and computes the result.
  * The following arithmetic operations are allowed: `+`, `-`, `*`, `/`, `^2(x)` and `sqr(x)`.
- * Check the right order by using parentheses. 
+ * Check the right order by using parentheses.
  * The result should be rounded off to the second decimal place.
  * The input are Strings like this:
- *  `"1+1"` = `2`
- *  `"10 * 20 + 30"` = `230`
- *  `"10 * (20 + 30)"`= `500`
- *  ... complete the tests!
- *
+ * `"1+1"` = `2`
+ * `"10 * 20 + 30"` = `230`
+ * `"10 * (20 + 30)"`= `500`
+ * ... complete the tests!
  */
 public class Exercise10 {
 
-    public int add(int a, int b) {
-        return a + b;
+
+    public String plus(String a, String b) {
+        return String.valueOf(Integer.valueOf(a) + Integer.valueOf(b));
     }
 
-    public double minus(double a, double b) {
-        return a - b;
+    public String minus(String a, String b) {
+        return String.valueOf(Integer.valueOf(a) - Integer.valueOf(b));
     }
 
-    public double multiply(int a, double b) {
-        return a * b;
+    public String multiply(String a, String b) {
+        return String.valueOf(Integer.valueOf(a) * Integer.valueOf(b));
     }
 
-    public double divide(double a, double b) {
-        return a/b;
+    public String divide(String a, String b) {
+        return String.valueOf(Integer.valueOf(a) / Integer.valueOf(b));
     }
 
-    public double run(String calculate) {
-
-        char[] calculateChars = calculate.toCharArray();
-
-        for(char c : calculateChars) {
-            if(Character.isDigit())
+    public int getLeftValuePos(String calculate, int operatorIndex) {
+        int i = (operatorIndex - 1);
+        for (; i >= 0; i--) {
+            if (Character.isDigit(calculate.charAt(i)) == false) break;
         }
+        return i;
+    }
+
+    public int getRightValuePos(String calculate, int operatorIndex) {
+        int i = (operatorIndex + 1);
+        for (; i < calculate.length(); i++) {
+            if (Character.isDigit(calculate.charAt(i)) == false) break;
+        }
+        return i;
+    }
+
+    public int getRightParenthesisPos(String calculate, int start) {
+        int i = calculate.length() - 1;
+        for (; i > start; i--) {
+            if (calculate.charAt(i) == ')') break;
+        }
+        return i;
+    }
+
+    public String calculate(String left, String right, char operator) {
+
+        String result = "";
+        switch (operator) {
+            case '*':
+                result = multiply(left, right);
+                break;
+            case '/':
+                result = divide(left, right);
+                break;
+            case '+':
+                result = plus(left, right);
+                break;
+            case '-':
+                result = minus(left, right);
+                break;
+        }
+        return result;
+    }
+
+    public String replaceResult(String calculate, String result, int left, int right) {
+        String temp = calculate;
+        calculate = temp.substring(0, left + 1) + result;
+        if (right < temp.length()) {
+            calculate += temp.substring(right);
+        }
+        return calculate;
+    }
+
+    public String handleSingleOperation(String calculate, char operator, int operatorIndex) {
+        // find values
+        int left = getLeftValuePos(calculate, operatorIndex);
+        int right = getRightValuePos(calculate, operatorIndex);
+        String leftStr = calculate.substring(left + 1, operatorIndex);
+        String rightStr = calculate.substring(operatorIndex + 1, right);
+
+        // calculate
+        String result = calculate(leftStr, rightStr, operator);
+        calculate = replaceResult(calculate, result, left, right);
+
+        return calculate;
+    }
+
+    public String handleParenthesis(String calculate, int start) {
+        int right = getRightParenthesisPos(calculate, start);
+        String result = parse(calculate.substring(start + 1, right));
+        return replaceResult(calculate, result, start - 1, right + 1);
+    }
+
+    public String parse(String calculate) {
+
+        boolean hasOperator = true;
+        int i;
+        do {
+            if ((i = calculate.indexOf('(')) > -1) {
+                calculate = handleParenthesis(calculate, i);
+            } else if ((i = calculate.indexOf('*')) > -1) {
+                calculate = handleSingleOperation(calculate, '*', i);
+            } else if ((i = calculate.indexOf('/')) > -1) {
+                calculate = handleSingleOperation(calculate, '/', i);
+            } else if ((i = calculate.indexOf('+')) > -1) {
+                calculate = handleSingleOperation(calculate, '+', i);
+            } else if ((i = calculate.indexOf('-')) > -1) {
+                calculate = handleSingleOperation(calculate, '-', i);
+            } else hasOperator = false;
+        } while (hasOperator);
+
+        return calculate;
+    }
+
+    public int run(String calculate) {
+
+        calculate = calculate.replaceAll("\\s", "");
+
+        return Integer.valueOf(parse(calculate));
 
     }
 }
--- src/test/java/week01/exercise10/Exercise10TestValidation.java
+++ src/test/java/week01/exercise10/Exercise10TestValidation.java
@@ -10,9 +10,9 @@ public class Exercise10TestValidation {
     public void calculateTest01() {
         Exercise10 tester = new Exercise10();
 
-        String calculate = "1 + 1";
+        String calculate = "1+1";
 
-        assertEquals("Wrong result!", 2, tester.run(calculate), 0);
+        assertEquals("Wrong result!", 2, tester.run(calculate));
     }
 
     @Test
@@ -21,15 +21,51 @@ public class Exercise10TestValidation {
 
         String calculate = "10 * 20 + 30";
 
-        assertEquals("Wrong result!", 230, tester.run(calculate), 0);
+        assertEquals("Wrong result!", 230, tester.run(calculate));
     }
 
     @Test
-    public void calculateTest03() {
+    public void calculateTest021() {
+        Exercise10 tester = new Exercise10();
+
+        String calculate = "30 + 10 * 20";
+
+        assertEquals("Wrong result!", 230, tester.run(calculate));
+    }
+
+    @Test
+    public void calculateTest022() {
+        Exercise10 tester = new Exercise10();
+
+        String calculate = "30 + 10 * 20 + 20";
+
+        assertEquals("Wrong result!", 250, tester.run(calculate));
+    }
+
+    @Test
+    public void parenthesis01() {
         Exercise10 tester = new Exercise10();
 
         String calculate = "10 * (20 + 30)";
 
-        assertEquals("Wrong result!", 500, tester.run(calculate), 0);
+        assertEquals("Wrong result!", 500, tester.run(calculate));
+    }
+
+    @Test
+    public void parenthesis03() {
+        Exercise10 tester = new Exercise10();
+
+        String calculate = "(10 * (20 + 30))";
+
+        assertEquals("Wrong result!", 500, tester.run(calculate));
+    }
+
+    @Test
+    public void parenthesis04() {
+        Exercise10 tester = new Exercise10();
+
+        String calculate = "(10 * (20 + 30)) / 2";
+
+        assertEquals("Wrong result!", 250, tester.run(calculate));
     }
 }
