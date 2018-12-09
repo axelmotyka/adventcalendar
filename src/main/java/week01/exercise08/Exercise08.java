f35be84f0ca3dd549c1e4d3655d2d3de52e4e58b
[flesch] <[friedl.lesch@telekom.de]>
2018-12-09 22:07:23 +0100

    Door8 solved

--- src/main/java/week01/exercise08/Exercise08.java
+++ src/main/java/week01/exercise08/Exercise08.java
@@ -6,10 +6,35 @@ package week01.exercise08;
  *
  * Sort the int array by using your own implementation of an BubbleSort algorithm.
  */
+
 public class Exercise08 {
 
     public int[] run(int[] intArray) {
-        return new int[] {};
-    }
 
-}
+    boolean swapped=false;
+    //int[] swappedArray = new int[intArray.length];
+    while (swapped == false){
+        for (int j=1; j< intArray.length; j++){
+        for (int i=0; i< intArray.length-j; i++){
+            int x = i+1;
+
+
+            int a = intArray[i];
+            int b = intArray[x];
+            /* if this pair is out of order */
+            if (a > b) {
+                /* swap them and remember something changed */
+                intArray[x]=a;
+                intArray[i]=b;
+            }
+            else {
+                continue;
+            }
+        }
+            swapped = true;
+        }
+
+    }
+    return intArray;
+
+}}
