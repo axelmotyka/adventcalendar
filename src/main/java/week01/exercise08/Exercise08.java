140bd46f9ff1cfe911ba8581f62656d6e5195c94
fldtag <lutzf@telekom.de>
2018-12-10 12:29:51 +0100

    Exercise 8

--- src/main/java/week01/exercise08/Exercise08.java
+++ src/main/java/week01/exercise08/Exercise08.java
@@ -9,7 +9,24 @@ package week01.exercise08;
 public class Exercise08 {
 
     public int[] run(int[] intArray) {
-        return new int[] {};
+        boolean hadToSort = true;
+        int sortCounter = 0;
+        while (hadToSort) {
+            for (int i = 1; i < intArray.length; ++i) {
+                int a = intArray[i - 1];
+                int b = intArray[i];
+                if (a > b) {
+                    intArray[i - 1] = b;
+                    intArray[i] = a;
+                    hadToSort = true;
+                    sortCounter += 1;
+                    break;
+                } else {
+                    hadToSort = false;
+                }
+            }
+        }
+        System.out.println(String.format("Sorted %s times.",sortCounter));
+        return intArray;
     }
-
 }
