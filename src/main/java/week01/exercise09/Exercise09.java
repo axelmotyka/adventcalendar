c5708208ef7347e17347c439356dfe177c89e337
[flesch] <[friedl.lesch@telekom.de]>
2018-12-10 10:12:23 +0100

    Door9 solved

--- src/main/java/week01/exercise09/Exercise09.java
+++ src/main/java/week01/exercise09/Exercise09.java
@@ -16,6 +16,54 @@ public class Exercise09 {
     public String run(String text) {
         log.info(String.format("Run(\"%s\")", text));
 
-        return new String();
+int y = 0;
+        char[] textChars= new char[text.length()];
+        for (int j = 0; j < text.length() ; j++) {
+            if (text.charAt(j)==' '){
+                continue;
+            }
+            else{
+                textChars[y]=text.charAt(j);
+                y++;
+                continue;
+            }
+
+        }
+
+        boolean swapped=false;
+        while (swapped == false){
+            for (int j=1; j< textChars.length; j++){
+                for (int i=0; i< textChars.length-j; i++){
+                    int x = i+1;
+
+
+                    char a = textChars[i];
+                    char b = textChars[x];
+                    //* if this pair is out of order *//*
+                    if (a > b) {
+                        //* swap them and remember something changed *//*
+                        textChars[x]=a;
+                        textChars[i]=b;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                swapped = true;
+            }
+
+        }
+        String result = "";
+        for (int i = 0; i < textChars.length; i++) {
+            if (textChars[i]=='\u0000'){
+                continue;
+            }
+            else{
+                result += textChars[i];
+            }
+
+        }
+
+        return result;
     }
 }
