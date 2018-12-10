a7ba6f5a0a07cf15d6a5c06c8cf4a33cf3912bf3
MesutCevik <mesut.cevik@gmx.de>
2018-12-10 16:12:13 +0100

    Door 5 is open.

--- src/main/java/week01/exercise05/Exercise05.java
+++ src/main/java/week01/exercise05/Exercise05.java
@@ -1,5 +1,8 @@
 package week01.exercise05;
 
+import com.sun.xml.internal.fastinfoset.stax.events.CharactersEvent;
+
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 /**
@@ -10,12 +13,47 @@ import java.util.logging.Logger;
  * Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string.
  */
 public class Exercise05 {
-    private static final Logger log = Logger.getLogger( Exercise05.class.getName() );
+    private static final Logger log = Logger.getLogger(Exercise05.class.getName());
+
+    // char[] vowels = {'a', 'e', 'i', 'o', 'u'};
 
     public String run(String text) {
         log.info(String.format("Run(\"%s\")", text));
+
+        // Split the text being passed (with "text") to letters and put them in a list variable "characters".
+        char[] characters;
+        characters = text.toCharArray();
+
+        // Iterate through each character in the array "characters".
+        for (int i = 0; i < characters.length; i++) {
+            char character = characters[i];
+            // Skip punctuation and blanks.
+            if (Character.isSpaceChar(character)) {
+                continue;
+            } else {
+                if (Character.isLetter(character)) {
+                    // Check up the characters ASCII code and take it to find the next character.
+                    int characterAscii = character;
+                    // Add 1 to characterAscii in order to create the ASCII code for the next character.
+                    int characterNextAscii = characterAscii + 1;
+                    char characterNext = (char) characterNextAscii;
+                    characters[i] = characterNext;
+                }
+            }
+        }
+        for (int j = 0; j < characters.length; j++) {
+            char characterToUpper = characters[j];
+
+            if (Arrays.asList('a', 'e', 'i', 'o', 'u').contains(characterToUpper)) {
+                characters[j] = Character.toUpperCase(characterToUpper);
+            }
+            }
+        return new String(characters);
+    }
+}
+
 /*
-1) Slice the text being passed into characters and put them in a list variable with characters "textChar".
+
 2) Iterate through "textChar" and proof, if a character is from the set [a-z] or [A-Z].
 (if(Character.isLetter(textChar[i])))
 3) If yes, than replace the character through the character, which follows it in the alphabet.
@@ -26,6 +64,3 @@ buchstaben[i] = Character.toUpperCase(buchstaben[i]))
 6) At least concatenate the changed characters to new string "textChanged".
 7) Return the result and print it out.
  */
-        return text;
-    }
-}
