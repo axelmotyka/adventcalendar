f557597bdc7fbb4b7e95fa65490997f1f4c22afe
fldtag <lutzf@telekom.de>
2018-12-10 13:06:25 +0100

    Exercise 8

--- src/test/java/week01/exercise08/Exercise08Test.java
+++ src/test/java/week01/exercise08/Exercise08Test.java
@@ -14,4 +14,13 @@ public class Exercise08Test {
 
         assertArrayEquals("Wrong sorting!", expected, tester.run(actual));
     }
+
+    @Test
+    public void run1() {
+        Exercise08 tester = new Exercise08();
+        int[] actual = new int[] {12, 9, 6, 18, 9, 9, 3, 15, 21, 9};
+        int[] expected = new int[] {3, 6, 9, 9, 9, 9, 12, 15, 18, 21};
+
+        assertArrayEquals("Wrong sorting!", expected, tester.run(actual));
+    }
 }
