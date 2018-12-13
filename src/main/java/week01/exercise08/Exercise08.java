3983adc6ad3f7ebc502d27654010e037522e8b94
MesutCevik <mesut.cevik@gmx.de>
2018-12-13 09:50:25 +0100

    Door 8 open.

--- src/main/java/week01/exercise08/Exercise08.java
+++ src/main/java/week01/exercise08/Exercise08.java
@@ -9,7 +9,29 @@ package week01.exercise08;
 public class Exercise08 {
 
     public int[] run(int[] intArray) {
-        return new int[] {};
-    }
+        boolean notAnymoreChanged;
 
+        do{
+            notAnymoreChanged = true;
+
+            for (int i = 0; i < intArray.length; i++) {
+                boolean notTheBorder = i + 1 < intArray.length;
+
+                if (notTheBorder) {
+                    boolean valuesChanged = (intArray[i] > intArray[i + 1]);
+
+                    if (valuesChanged) {
+                        int temp = intArray[i];
+                        intArray[i] = intArray[i + 1];
+                        intArray[i + 1] = temp;
+
+                        notAnymoreChanged = false;
+                    }
+                }
+            }
+        }while(!notAnymoreChanged);
+    return intArray;
+    }
 }
+
+
