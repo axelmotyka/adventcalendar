66863d313ba93fe9660714d0230395387f63cbe7
fldtag <lutzf@telekom.de>
2018-12-13 12:19:18 +0100

    Exercise 9 Hauptteil nicht selbstständig ausgedacht

--- src/main/java/week01/exercise09/Exercise09.java
+++ src/main/java/week01/exercise09/Exercise09.java
@@ -11,11 +11,44 @@ import java.util.logging.Logger;
  * Implement your own QuickSort.
  */
 public class Exercise09 {
-    private static final Logger log = Logger.getLogger( Exercise09.class.getName() );
+    private static final Logger log = Logger.getLogger(Exercise09.class.getName());
 
     public String run(String text) {
         log.info(String.format("Run(\"%s\")", text));
 
-        return new String();
+        text = text.replaceAll("[^A-Za-z0-9]", "");
+        char charArray [] = text.toCharArray();
+        quickSort(charArray,0, charArray.length-1);
+        return String.valueOf(charArray);
+    }
+
+    // Lösung ab hier im Wesentlichen kopiert von https://www.baeldung.com/java-quicksort
+    // Hab's zwar kapiert, kann es aber nicht freihändig programmieren
+
+    private char [] quickSort(char charArray [], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(charArray, begin, end);
+            quickSort(charArray, begin, partitionIndex-1);
+            quickSort(charArray, partitionIndex+1, end);
+            }
+        return charArray;
+        }
+
+    private int partition(char charArray [], int begin, int end) {
+        int pivot = charArray[end];
+        int i = (begin-1);
+        for (int j = begin; j < end; j++) {
+            if (charArray[j] <= pivot) {
+                i++;
+
+                char swapTemp = charArray[i];
+                charArray[i] = charArray[j];
+                charArray[j] = swapTemp;
+            }
+        }
+        char swapTemp = charArray[i+1];
+        charArray[i+1] = charArray[end];
+        charArray[end] = swapTemp;
+        return i+1;
     }
 }
