4639d12028d90783892c9ff6c83a017a80dd5790
Halil Coban <halil.coban@telekom.de>
2018-12-14 11:19:26 +0100

    Halil Coban Tag 11 ... ich arbeite noch dran

---
+++ src/main/java/week02/exercise11/Driver.java
@@ -0,0 +1,29 @@
+package week02.exercise11;
+
+public class Driver {
+
+    public String lastName;
+    public String firstName;
+    public int startingNumber;
+
+    public Driver(String lastName , String firstName,
+                  int startingNumber){
+        this.lastName = lastName ;
+        this.firstName = firstName ;
+        this.startingNumber =  startingNumber;
+
+    }
+
+    public String getName(){
+        return this.lastName + "," + this.firstName;
+    }
+
+    public int getStartingNumber(){
+        return this.startingNumber;
+    }
+
+    public String toString(){
+        return "(" + this.startingNumber + ") " + firstName + " " + lastName;
+
+    }
+}
---
+++ src/test/java/week02/exercise11/Exercise11Test.java
@@ -0,0 +1,87 @@
+package week02.exercise11;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+import static week02.exercise11.Vehicle.engineType.*;
+
+public class Exercise11Test {
+
+    @Test
+    public void Test01() {
+        Driver tester = new Driver("Schumacher", "Michael", 1);
+
+
+
+        assertEquals("Wrong result!", "Schumacher,Michael", tester.getName());
+    }
+
+    @Test
+    public void Test02() {
+        Driver tester = new Driver("Schumacher", "Michael", 1);
+
+
+        assertEquals("Wrong result!", 1, tester.getStartingNumber());
+    }
+
+    @Test
+    public void Test03() {
+        Driver tester = new Driver("Schumacher", "Michael", 1);
+
+
+
+        assertEquals("Wrong result!", "(1) Michael Schumacher", tester.toString());
+    }
+
+    @Test
+    public void Test04() {
+        Vehicle tester = new Vehicle("Toyota", "Verso", 100, gasoline);
+
+
+
+        assertEquals("Wrong result!", "Toyota", tester.getManufacturer());
+    }
+
+    @Test
+    public void Test05() {
+        Vehicle tester = new Vehicle("Toyota", "Verso", 100, gasoline);
+
+
+
+        assertEquals("Wrong result!", "Verso", tester.getName());
+    }
+
+    @Test
+    public void Test06() {
+        Vehicle tester = new Vehicle("Toyota", "Verso", 100, gasoline);
+
+
+
+        assertEquals("Wrong result!", 100, tester.getHorsepower());
+    }
+
+
+    @Test
+    public void Test07() {
+        Vehicle tester = new Vehicle("Toyota", "Verso", 100, gasoline);
+
+
+
+        assertEquals("Wrong result!", Vehicle.engineType.gasoline, tester.getEngineType());
+    }
+
+    @Test
+    public void Test08() {
+        Vehicle tester = new Vehicle("Toyota", "Verso", 100, gasoline);
+
+
+
+        assertEquals("Wrong result!", "Toyota Verso,"
+                + Vehicle.engineType.gasoline + "," + 100
+                + "hps", tester.toString());
+    }
+
+
+
+}
