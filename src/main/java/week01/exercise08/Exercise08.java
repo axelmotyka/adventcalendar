2059b7ef40ea8ed63e256bb0569f757e7d4c9e21
MesutCevik <mesut.cevik@gmx.de>
2018-12-17 11:29:44 +0100

    Added comments for myself.

--- src/main/java/week01/exercise08/Exercise08.java
+++ src/main/java/week01/exercise08/Exercise08.java
@@ -9,23 +9,24 @@ package week01.exercise08;
 public class Exercise08 {
 
     public int[] run(int[] intArray) {
+        // This boolean variable is needed to run the do-while-loop.
         boolean notAnymoreChanged;
 
         do{
-            notAnymoreChanged = true;
+            notAnymoreChanged = true; // Set the boolean variable to the value "true".
 
             for (int i = 0; i < intArray.length; i++) {
-                boolean notTheBorder = i + 1 < intArray.length;
+                boolean notTheBorder = i + 1 < intArray.length; // Don´t execute the code below, when i is the end-border.
 
                 if (notTheBorder) {
-                    boolean valuesChanged = (intArray[i] > intArray[i + 1]);
+                    boolean valuesChanged = (intArray[i] > intArray[i + 1]); // Check whether a number is greater than the following number.
 
-                    if (valuesChanged) {
+                    if (valuesChanged) { // If compared values (numbers) are changed, then sort them to the right order.
                         int temp = intArray[i];
                         intArray[i] = intArray[i + 1];
                         intArray[i + 1] = temp;
 
-                        notAnymoreChanged = false;
+                        notAnymoreChanged = false; // Change the value to "false" so the do-while-loop can continue.
                     }
                 }
             }
