0b349680d2f36d32be693bd5e9b8a1ac7c43f394
[flesch] <[friedl.lesch@telekom.de]>
2018-12-18 22:42:24 +0100

    Door 10 solved

--- src/main/java/week01/exercise10/Exercise10.java
+++ src/main/java/week01/exercise10/Exercise10.java
@@ -1,8 +1,7 @@
 package week01.exercise10;
 
 import java.util.ArrayList;
-import java.util.ArrayList.*;
-import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Closest Enemy
@@ -19,82 +18,173 @@ import java.util.List;
 public class Exercise10 {
     public int a;
     public int b;
-    //public String operator;
     public String type;
-    public int result;
-    public int numberCount;
+    String result = "";
+
 
     public int run(String calculate) {
+        int numberCount = 0;
 
-
-        //String[] newCalculate = {"+", "-", "*", "/"};
-        //calculate.replaceAll(" ", "");
-        //int x = 0;
         String[] newCalculate = calculate.split("");
         ArrayList<String> calculateArrayList = new ArrayList<>();
 
-        //String[] newCalculate = calculate.split("[\\d]");
-        //String[] newCalculate = calculate.split("[^\\p{S}\\d]");
-        //String[] numbers = calculate.split("[[\\p{Punct}]]");
-        //System.out.println(numbers);
-        System.out.println(newCalculate);
-        for (int j = 0; j < newCalculate.length; j++) {
-            if (newCalculate[j] != "[0-9]" ||
-                    newCalculate[j] != "+" ||
-                    newCalculate[j] != "-" ||
-                    newCalculate[j] != "*" ||
-                    newCalculate[j] != "/" ||
-                    newCalculate[j] != "(" ||
-                    newCalculate[j] != ")") {
-                calculateArrayList.add(newCalculate[j]);
-            } else {
+        //System.out.println(newCalculate);
+        for (String word : newCalculate) {
+            if (Pattern.matches("[0-9]", word) ||
+                    word.equals("+") ||
+                    word.equals("-") ||
+                    word.equals("*") ||
+                    word.equals("/") ||
+                    word.equals("(") ||
+                    word.equals(")")) {
+                calculateArrayList.add(word);
             }
+/*            try {
+                if(!(calculateArrayList.size()%2==0)){
+                    boolean isOdd=true;
+                }
+            }
+            catch (ExceptionInInitializerError exception){
+                System.out.println("This is not a math term!");
+            }*/
+
             if (calculateArrayList.contains("(")) {
                 type = "brace";
+                numberCount = 0;
+            } else if (Pattern.matches("[0-9]", word) && numberCount != 0) {
+                int index = calculateArrayList.lastIndexOf(word) - 1;
+                String oldNum = calculateArrayList.get(index);
+                calculateArrayList.set(calculateArrayList.indexOf(word) - 1, oldNum += word);
+                calculateArrayList.remove(index + 1);
+            } else if (Pattern.matches("[0-9]", word)) {
+                numberCount++;
+                //continue;
             } else if (calculateArrayList.contains("*")) {
                 type = "multi";
             } else if (calculateArrayList.contains("/")) {
                 type = "div";
+                numberCount = 0;
             } else if (calculateArrayList.contains("+")) {
                 type = "add";
+                numberCount = 0;
             } else if (calculateArrayList.contains("-")) {
                 type = "min";
+                numberCount = 0;
             } else {
-/*                numberCount = +1;
-                a = ((char) (newCalculate[j - 1].charAt(0) - 48)) * 10;
-                b = ((char) (newCalculate[j].charAt(0) - 48));
-                calculateIndex.put("newNumber" + (j - 1), a + b);
-                //int newNumber=a+b;*/
-
-
+                numberCount = 0;
             }
-            type = "number";
-            continue;
-            //startOperation(plus);
+
 
         }
 
-        switch (type){
+        switch (type) {
             case "brace":
                 int start = calculateArrayList.indexOf("(");
                 int end = calculateArrayList.lastIndexOf(")");
-                calculateArrayList.subList(start,end).set(0, preCalculation(calculateArrayList.subList(start,end)));
+                if (calculateArrayList.subList(start, end).size() > 1) {
+                    String newMath = "";
+                    for (String word : calculateArrayList.subList(start+1, end)
+                    ) {
+                        newMath += word;
+                    }
+                    String result = braceCalculation(newMath);
+
+                    calculateArrayList.subList(start, end).set(0, result);
+                    while(calculateArrayList.size()>3) {
+                        calculateArrayList.remove(3);
+                    }
+                }
+
+            case "multi":
+                result = "";
+                int multi = calculateArrayList.indexOf("*");
+                int a = Integer.parseInt(calculateArrayList.get(multi - 1));
+                int b = Integer.parseInt(calculateArrayList.get(multi + 1));
+
+                int c = a * b;
+                result += c;
+
+                calculateArrayList.remove(multi);
+                calculateArrayList.remove(multi);
+                calculateArrayList.set(multi - 1, result);
+                if (calculateArrayList.size() > 1) {
+                    String newMath = "";
+                    for (String word : calculateArrayList
+                    ) {
+                        newMath += word;
+                    }
+                    run(newMath);
+                }
+                break;
+
+            case "div":
+                result = "";
+                int div = calculateArrayList.indexOf("/");
+                a = Integer.parseInt(calculateArrayList.get(div - 1));
+                b = Integer.parseInt(calculateArrayList.get(div + 1));
+
+                int d = a / b;
+                result += d;
+
+                calculateArrayList.remove(div);
+                calculateArrayList.remove(div);
+                calculateArrayList.set(div - 1, result);
+                if (calculateArrayList.size() > 1) {
+                    run(calculateArrayList.toString());
+                }
+                break;
+
             case "add":
-                int operator=calculateArrayList.indexOf("+");
-                a = calculateArrayList.get(operator-1).charAt(0);
-                b = calculateArrayList.get(operator+1).charAt(0);
-                result = a+b;
+                result = "";
+                int add = calculateArrayList.indexOf("+");
+                a = Integer.parseInt(calculateArrayList.get(add - 1));
+                b = Integer.parseInt(calculateArrayList.get(add + 1));
 
+                int e = a + b;
+                result += e;
+                calculateArrayList.remove(add);
+                calculateArrayList.remove(add);
+                calculateArrayList.set(add - 1, result);
+                if (calculateArrayList.size() > 1) {
+                    run((String) (calculateArrayList.toString()));
+                }
+                break;
 
+            case "minus":
+                result = "";
+                int minus = calculateArrayList.indexOf("-");
+                a = Integer.parseInt(calculateArrayList.get(minus - 1));
+                b = Integer.parseInt(calculateArrayList.get(minus + 1));
+
+                int f = a - b;
+                result += f;
+
+                calculateArrayList.remove(minus);
+                calculateArrayList.remove(minus);
+                calculateArrayList.set(minus - 1, result);
+                if (calculateArrayList.size() > 1) {
+                    run((String) (calculateArrayList.toString()));
+                }
+                break;
         }
-        return result;
+        return (Integer.parseInt(result));
     }
 
-    private String preCalculation(List<String> subList) {
+/*    public String braceCalculation(String newMath) {
         String result = "";
-        result += run(subList.toString());
+        newMath.replace("(","");
+        newMath.replace(")","");
+        result += run(newMath);
+        return result;
+    }*/
+
+
+
+    private String braceCalculation(String newMath) {
+        String result = "";
+
+        int a = run(newMath);
+        result += a;
         return result;
     }
-
-
 }
